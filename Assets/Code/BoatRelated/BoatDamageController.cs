73b0d5645b93f02933f6bcdb372471902f01ef39
agneaspg <agneaspg@playgroundsquad.com>
2019-09-17 12:16:27 +0200

    Implemented death condition, you can now die if your hull/HP reaches 0

--- Assets/Code/BoatRelated/BoatDamageController.cs
+++ Assets/Code/BoatRelated/BoatDamageController.cs
@@ -1,15 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BoatDamageController : MonoBehaviour
 {
     public int MaxHull = 1000;
     private float hull;
 
+    public UnityEvent onDeath;
+
     private void Start()
     {
         hull = MaxHull;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hull<=0)
+        {
+            Time.timeScale = 0;
+            onDeath.Invoke();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            hull -= 1;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)

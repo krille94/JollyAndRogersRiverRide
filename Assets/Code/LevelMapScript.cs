3b6cfaf2e97cafb80fce210d6baed034cf27e678
agneaspg <agneaspg@playgroundsquad.com>
2019-09-18 14:50:16 +0200

    Implemented functionality for an overhead map with an icon that shows how far along the level you are!

---
+++ Assets/Code/LevelMapScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMapScript : MonoBehaviour
+{
+    public GameObject boat;
+    public GameObject map;        // A plane showing the level's map
+    public GameObject startpoint; // Where the boat first spawns
+    public GameObject endpoint;   // The goal
+
+    float startZ;
+    float endZ;
+    public float mapStartZ;
+    public float mapEndZ;
+
+    public float levelLength;
+    public float mapLength;
+    Vector3 iconPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startZ = startpoint.transform.position.z;
+        endZ = endpoint.transform.position.z;
+
+        mapStartZ = -4;
+        mapEndZ = 4;
+        levelLength = endZ - startZ;
+        mapLength = mapEndZ - mapStartZ;
+
+        iconPos = transform.localPosition;
+        iconPos.z = mapEndZ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float boatPos = (endZ - boat.transform.position.z) / levelLength;
+        iconPos.z = mapStartZ + (boatPos * mapLength);
+        transform.localPosition = new Vector3(1, 1, iconPos.z);
+    }
+}

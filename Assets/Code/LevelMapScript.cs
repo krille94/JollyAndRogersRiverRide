80bfbd40b8ea59b8c14a1424ffa66af07113858e
agneaspg <agneaspg@playgroundsquad.com>
2019-10-01 12:53:25 +0200

    Set mini-map to work with turns, fixed error in menu script

--- Assets/Code/LevelMapScript.cs
+++ Assets/Code/LevelMapScript.cs
@@ -9,24 +9,44 @@ public class LevelMapScript : MonoBehaviour
     public GameObject startpoint; // Where the boat first spawns
     public GameObject endpoint;   // The goal
 
-    float startZ;
-    float endZ;
+    Vector3 start;
+    Vector3 end;
     float mapStartZ;
     float mapEndZ;
 
+    Vector3 levelLayout;
     float levelLength;
     float mapLength;
     Vector3 iconPos;
+    int amountOfNodes;
+
+    private RiverController river;
 
     // Start is called before the first frame update
     void Start()
     {
-        startZ = startpoint.transform.position.z;
-        endZ = endpoint.transform.position.z;
+        river = RiverController.instance;
 
-        mapStartZ = -4;
-        mapEndZ = 4;
-        levelLength = endZ - startZ;
+        //Vector3 target = river.riverAsset.GetNodeFromPosition(river.transform.position, body.transform.position).centerVector;
+
+        // = river.riverAsset.GetNodeFromPosition(river.transform.position, startpoint.transform.position).centerVector;
+        //end = river.riverAsset.GetNodeFromPosition(river.transform.position, endpoint.transform.position).centerVector;
+        start=river.riverAsset.nodes[0].centerVector;
+
+        int i = 0;
+        foreach(RiverNode node in river.riverAsset.nodes)
+        {
+            levelLayout += node.centerVector;
+            i++;
+            if (i == 14)
+                break;
+        }
+        amountOfNodes = i;
+        //end = river.riverAsset.nodes[i-1].centerVector;
+        end = river.riverAsset.GetNodeFromPosition(river.transform.position, endpoint.transform.position).centerVector;
+        mapStartZ = 4;
+        mapEndZ = -4;
+        levelLength = (levelLayout.x + levelLayout.z) / 2;
         mapLength = mapEndZ - mapStartZ;
 
         iconPos = transform.localPosition;
@@ -36,9 +56,26 @@ public class LevelMapScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float boatPos = (endZ - boat.transform.position.z) / levelLength;
+        RiverNode boatLocation = river.riverAsset.GetNodeFromPosition(river.transform.position, boat.transform.position);
 
-        if(PlayerData.distanceTraveled<boatPos) PlayerData.distanceTraveled=boatPos;
+        float boatPos = 0;
+        int i = 0;
+        foreach (RiverNode node in river.riverAsset.nodes)
+        {
+            boatPos += ((node.centerVector.x + node.centerVector.z) / 2);
+
+            if (node==boatLocation)
+            {
+                //float alter = ((node.centerVector.x - boat.transform.position.x) + (node.centerVector.z - boat.transform.position.z));
+                //boatPos -= alter;
+                break;
+            }
+        }
+
+        boatPos /= levelLength;
+
+        if (PlayerData.distanceTraveled<boatPos) PlayerData.distanceTraveled=boatPos;
+
         iconPos.z = mapStartZ + (boatPos * mapLength);
         transform.localPosition = new Vector3(1, 1, iconPos.z);
     }

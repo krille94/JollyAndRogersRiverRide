c6ee5bac4294e9ffc4b0752810789284575beb01
agneaspg <agneaspg@playgroundsquad.com>
2019-10-01 10:53:02 +0200

    Adjusted and implemented boat knockback code (requires bugtesting and polish, but at least you won't get stuck on walls anymore)

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -19,6 +19,8 @@ public class GameController : MonoBehaviour
         DontDestroyOnLoad(gameObject);
 
         Time.timeScale = 1;
+
+        UserSettings.ReadSettings();
     }
 
     public void OnCompletedLevel()

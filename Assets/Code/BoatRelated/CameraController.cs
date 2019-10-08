b2260a31eb96aa8300665dab9d240f2c64c73867
krille t <kristian.timmermand@gmail.com>
2019-10-08 17:03:40 +0200

    no message

--- Assets/Code/BoatRelated/CameraController.cs
+++ Assets/Code/BoatRelated/CameraController.cs
@@ -7,21 +7,61 @@ public class CameraController : MonoBehaviour
     public RiverController riverController;
     public Transform boat;
 
-    public Vector3 offset;
-    public float viewRot;
+    public Vector3 offsetPosition;
+    public float offsetAngle;
 
-    public float speed = 0.25f;
+    public float translationSpeed = 0.25f;
+    public float rotationSpeed = 0.5f;
 
-    public Vector3 targetPosition;
+    private Vector3 targetPosition;
+
+    private Quaternion targetRotation;
+
+    private RiverNode targetNode;
+
+    [SerializeField] private bool newTarget = true;
+
+    Skybox skybox;
+    private void Start()
+    {
+        skybox = GetComponent<Skybox>();
+    }
 
     void Update()
     {
+        if(skybox != null)
+        {
+            skybox.material.SetFloat("_Rotation", Time.time);
+        }
+
+        //Gets target position
         RiverNode node = riverController.riverAsset.GetNodeFromPosition(boat.position);
+        if(node != targetNode)
+        {
+            targetNode = node;
 
-        targetPosition = node.centerVector + offset;
+            targetPosition = targetNode.centerVector + offsetPosition;
+            Quaternion temp = transform.rotation;
+            transform.LookAt(targetPosition);
+            targetRotation = Quaternion.Euler(45 + offsetAngle, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+            transform.rotation = temp;
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+            newTarget = true;
+        }
 
-        transform.LookAt(boat);
+        if(newTarget)
+        {
+            //changes location
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * translationSpeed);
+
+            //changes rotation
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
+            //post changes rotation angle
+            //transform.rotation = Quaternion.Euler(45 + offsetAngle, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+            if (Vector3.Distance(transform.position, new Vector3(targetPosition.x, transform.position.y, targetPosition.z)) < Mathf.Abs(offsetPosition.z))
+                newTarget = false;
+        }
     }
 }

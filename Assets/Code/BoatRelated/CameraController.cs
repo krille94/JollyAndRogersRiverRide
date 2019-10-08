b5daa9a3ae79254c70fe600d55638e3f91d2f4b2
agneaspg <agneaspg@playgroundsquad.com>
2019-10-08 17:01:12 +0200

    Partially fixed camera controller

--- Assets/Code/BoatRelated/CameraController.cs
+++ Assets/Code/BoatRelated/CameraController.cs
@@ -11,17 +11,50 @@ public class CameraController : MonoBehaviour
     public float viewRot;
 
     public float speed = 0.25f;
+    public float rotationSpeed = 200f;
 
     public Vector3 targetPosition;
+    public Vector3 targetRotation;
+    public Vector3 boatOffset;
+
+    private void Start()
+    {
+        transform.rotation = boat.transform.rotation;
+    }
 
     void Update()
     {
         RiverNode node = riverController.riverAsset.GetNodeFromPosition(boat.position);
 
-        targetPosition = node.centerVector + offset;
+        //Quaternion flowRot = Quaternion.Euler(targetRotation.x, targetRotation.y, targetRotation.z);
+        //Vector3 myVector = Vector3.one;
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+        Quaternion rotation = Quaternion.Euler(targetRotation.x, targetRotation.y, targetRotation.z);
+        boatOffset = rotation * offset;
 
-        transform.LookAt(boat);
+        targetPosition = boat.position + boatOffset;
+        targetRotation = node.flowDirection;
+
+        transform.position = targetPosition;
+        // Rotate by node direction around Y pos
+
+        //Vector3 newRot = Vector3.RotateTowards(transform.forward, targetRotation, 10, 0.0f);
+        //transform.rotation = Quaternion.Euler(newRot);
+        transform.Rotate(new Vector3(-viewRot, 0, 0));
+        //transform.rotation = Quaternion.LookRotation(node.flowDirection, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(node.flowDirection), Time.deltaTime);
+        transform.Rotate(new Vector3(viewRot,0,0));
+
+        //transform.rotation = Quaternion.Euler(targetRotation.x, targetRotation.y, targetRotation.z);
+        //if (transform.rotation != Quaternion.Euler(targetRotation))
+        //transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+
+        /// OLD CODE
+        /// 
+        //targetPosition = node.centerVector + offset;
+
+        //transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * speed);
+
+        //transform.LookAt(boat);
     }
 }

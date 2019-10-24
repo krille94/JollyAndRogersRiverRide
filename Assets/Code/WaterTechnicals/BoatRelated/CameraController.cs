40b0deaf10efb2eab0839597f6cfe907ab0480d4
agneaspg <agneaspg@playgroundsquad.com>
2019-10-24 14:03:17 +0200

    Fixed end point, fixed collision-less wall section

--- Assets/Code/WaterTechnicals/BoatRelated/CameraController.cs
+++ Assets/Code/WaterTechnicals/BoatRelated/CameraController.cs
@@ -322,7 +322,7 @@ public class CameraController : MonoBehaviour
     private void OnGUI()
     {
         //GUI.Box(new Rect(0, 0, 100, 25), "HULL: " + hull.ToString("F0") + " / " + MaxHull.ToString());
-        if (blocked)
-            GUI.Box(new Rect(0, 50, 100, 25), "Boat obscured");
+        //if (blocked)
+            //GUI.Box(new Rect(0, 50, 100, 25), "Boat obscured");
     }
 }

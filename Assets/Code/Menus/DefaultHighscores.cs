aae83272835dc8517c1b62738a71738b728c2edf
agneaspg <agneaspg@playgroundsquad.com>
2019-11-08 14:44:17 +0100

    Added manual implementation for default saves

---
+++ Assets/Code/Menus/DefaultHighscores.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultHighscores : MonoBehaviour
+{
+    public List<Highscore> defaultSaves = new List<Highscore>();
+}

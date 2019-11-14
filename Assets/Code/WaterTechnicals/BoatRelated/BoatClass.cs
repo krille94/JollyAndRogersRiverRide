0286477f645c1df4d1cd16650f11fc524b79a8f8
agneaspg <agneaspg@playgroundsquad.com>
2019-11-14 18:37:00 +0100

    FINAL fix

--- Assets/Code/WaterTechnicals/BoatRelated/BoatClass.cs
+++ Assets/Code/WaterTechnicals/BoatRelated/BoatClass.cs
@@ -376,7 +376,7 @@ public class BoatClass : FloatingObject
     private void OnGUI()
     {
         //GUI.Box(new Rect(0, 0, 100, 25), "HULL: " + hull.ToString("F0") + " / " + MaxHull.ToString());
-        if (invincible)
-            GUI.Box(new Rect(0, 0, 100, 25), "Invincible");
+        //if (invincible)
+        //    GUI.Box(new Rect(0, 0, 100, 25), "Invincible");
     }
 }
